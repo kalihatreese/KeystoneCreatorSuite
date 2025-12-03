8e13b8e9dbafe7e56d8a1ff006ccf5f35c3a2db1
Reese <kalihatreese+resonance@users.noreply.github.com>
2025-12-03 12:36:10 -0500

    Prepare for Cloudflare Pages deploy

--- build.sh
+++ build.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-if command -v wrangler >/dev/null 2>&1; then
-    wrangler publish
-elif [ -f package.json ]; then
-    npm install && npm run build
-elif [ -f makefile ]; then
-    make
-elif [ -f setup.py ]; then
-    python setup.py build
-else
-    echo "No build needed"
-fi
+
+echo "No build needed"
